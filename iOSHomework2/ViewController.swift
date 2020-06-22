d49f561ffb6da1e060e631e3132f8a18354b9868
hayaalameeri <hayoony.m@gmail.com>
2020-06-23 01:25:53 +0300

    hw + bonus  completed

--- iOSHomework2/ViewController.swift
+++ iOSHomework2/ViewController.swift
@@ -19,8 +19,7 @@ class ViewController: UIViewController {
         let member = nameTextField.text!
         
         // MARK: -   1️⃣ تحت الخط membersNamesArray إلي المصفوفه memberقم بإضافة المتغير
-        
-        
+        membersNamesArray.append(member)
         
         // MARK: -   النهاية
         
@@ -34,7 +33,7 @@ class ViewController: UIViewController {
         
         // MARK: -  4️⃣ functionCall داخل المتغير  secretNameLetter قم باستدعاء الدالة
         
-        var functionCall = ""
+        var functionCall = secretNameLetter(membernamearray: membersNamesArray)
         
         
         // MARK: -   النهاية
@@ -52,7 +51,7 @@ class ViewController: UIViewController {
         
         // MARK: -  5️⃣ functionCall داخل المتغير  secretNameEmoji قم باستدعاء الدالة
         
-        var functionCall = ""
+        var functionCall = secretNametoEmoji(membernamearray: membersNamesArray)
         
         // MARK: -   النهاية
         
@@ -65,8 +64,18 @@ class ViewController: UIViewController {
     
     
     // MARK: - 2️⃣ تحت هذا الخط secretNameLetter قم بكتابة الداله
+    var name = ""
     
+    func secretNameLetter (membernamearray:[String])-> String
+    {
+            for i in 0..<membersNamesArray.count {
+                name += String(membersNamesArray[i].prefix(1))
+            }
+            return name
+        
+        }
     
+
     // MARK: -   النهاية
     
     
@@ -74,11 +83,40 @@ class ViewController: UIViewController {
     
     
     // MARK: - 3️⃣ تحت هذا الخط secretNameEmoji قم بكتابة الداله
-    
-    
+   
+    var emojisWithLetter = ["A":"💜","B":"💧","C":"☔️","D":"🌈","E":"🏄‍♂️","F":"🪁","G":"⛸","H":"🥌","I":"🎣","J":"💽","K":"🏛","L":"🚩","M": "🕓","N":"🎊","O":"🧸","P":"🖼","Q":"🛍","R":"💝","S":"☯️","T":"🟣","U":"♥️","V":"🌅","W":"◼️","X":"🧩","Y":"🎹","Z":"🖥"]
+    var finalstring = ""
+    var firstchar = ""
+    func secretNametoEmoji (membernamearray:[String])-> String {
+       for i in membersNamesArray {
+
+        firstchar =  (String(i.prefix(1)).uppercased())
+        for (letter, emoji) in emojisWithLetter {
+            if letter == firstchar {
+                finalstring.append(emoji)}
+        }
+        }
+        return finalstring
+        
+    }
+
+ 
     // MARK: -   النهاية
     
-    
-    
+    // bonus
+
+        func encryptWith(emoji : Bool, array : [ String]) -> String {
+            if emoji{
+                return secretNametoEmoji(membernamearray: membersNamesArray)}
+                else{
+                return secretNameLetter(membernamearray: membersNamesArray)}
+}
+
+
+
+
+
+
+
 }
 
